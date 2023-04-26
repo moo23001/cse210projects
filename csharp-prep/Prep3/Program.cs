81364d9edf2bfb8c499b4a03fa47f20521194753
moo23001 <moo23001@byui.edu>
2023-04-25 18:05:31 -0600

    Completed Prep3 with stretch assigments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string answer;
+        
+        do {
+            Random randomNumber = new Random();
+            int magicNumber = randomNumber.Next(1,101);
+            int guess = -1;
+            int attempts = 0;
+            do 
+            {
+                Console.Write("Guess the magic number: ");
+                String userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                attempts++;
+            } while (guess != magicNumber);
+            
+            Console.WriteLine($"It took you {attempts} trys to guess the magic number");
+
+            
+            Console.Write("Do you wish to play again (y/n) ");
+            answer = Console.ReadLine();
+        } while (answer == "y");
+
     }
 }
