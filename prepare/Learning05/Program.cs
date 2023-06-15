66ddb334237c4c1feb98025b062b458ad093d6e8
pedromoo <moo23001@byui.edu>
2023-06-14 21:47:34 -0600

    Learning 5 activity added

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s1 = new Square("blue", 2);
+
+        Rectangle r1 = new Rectangle("pink", 2, 3);
+
+        Circle c1 = new Circle("white", 2);
+
+        List<Shape> sList = new List<Shape>();
+        sList.Add(s1);
+        sList.Add(r1);
+        sList.Add(c1);
+        foreach (Shape s in sList){
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
+
     }
+    
+    
 }
